1330dae336ff7a865523926745824c8424952e58
Romie Greenbourg <informathemusic@gmail.com>
2022-09-07 09:00:28 +0200

    Add demojify
    Can be useful to detect the actual length of text before sending

--- index.js
+++ index.js
@@ -63,21 +63,25 @@ module.exports.getShortcode = (emoji, addColons = true) => {
  */
 module.exports.emojify = str => {
 
-  //Get all shortcodes
-  var shortcodes = str.match(/:[^\s:]+:/g) || [];
+  // Replace all shortcodes
+  var result = str.replace(/:[^\s:]+:/g, (shortcode) => module.exports.getEmoji(shortcode));
 
-  //Replace the shortcodes
-  for (let shortcode of shortcodes) {
-    try {
-      str = str.replace(shortcode, module.exports.getEmoji(shortcode));
-    } catch (e) {
+  return result;
+}
+/**
+ * Take a given string and convert all emoji characters found in it to shortcodes. If no emojis are found, it returns the original string.
+ * 
+ * @param {string} str The string to convert all emojis to shortcode.
+ * @returns {string} The string after converting all emojis within it to shortcodes.
+ */
+module.exports.demojify = str => {
+  const emojiRegex = /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g;
+  
+  
+  // Replace all shortcodes
+  var result = str.replace(emojiRegex, (emoji) => module.exports.getShortcode(emoji));
 
-      //Ignore errors
-      continue;
-    }
-  }
-
-  return str;
+  return result;
 }
 
 //Export error
