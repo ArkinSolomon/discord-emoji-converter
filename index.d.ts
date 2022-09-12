80cd0679a7dca2754dd2f7432bb5b5747153037b
Jerome Wang <informathemusic@gmail.com>
2022-09-12 20:42:44 +0200

    Add types for demojify

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,7 @@ export const emojis: Record<string, string>;
 export const getEmoji: (shortcode: string) => string;
 export const getShortcode: (emoji: string, addColons?: boolean) => string;
 export const emojify: (str: string) => string;
+export const demojify: (str: string) => string;
 
 export class EmojiError extends Error {
   public readonly name = 'EmojiError';
